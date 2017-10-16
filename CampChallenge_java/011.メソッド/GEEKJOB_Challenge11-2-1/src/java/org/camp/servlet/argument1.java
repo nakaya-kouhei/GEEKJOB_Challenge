66b1503faddd7d8eb81313814a7d4d160e89fc8c
nakaya-kouhei <northsea1224@gmail.com>
2017-10-16 18:22:33 +0900

    Java メソッド引数引数1課題 提出します

---
+++ CampChallenge_java/011.メソッド/GEEKJOB_Challenge11-2-1/src/java/org/camp/servlet/argument1.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+
+/**
+ *
+ * @author guest1Day
+ */
+
+public class argument1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    //  引数が偶数か奇数かを判定して表示
+    void check(int num, PrintWriter pw){
+
+        if(num % 2 == 0){
+            
+            pw.println(num + "は偶数です。<br>");
+            
+        }else{
+            
+            pw.println(num + "は奇数です。<br>");
+            
+        }
+        
+    }
+    
+    //  数値を渡されなかった場合は0として処理
+    void check(PrintWriter pw){
+
+        check(0, pw);
+        
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            Random rand = new Random();
+            //  適当な数値を取得            
+            int num = rand.nextInt();
+            
+            //  ユーザー定義メソッドを呼び出し
+            check(num, out);
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
