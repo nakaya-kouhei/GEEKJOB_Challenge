d6c05d99b2761a0dba284aea676d55c4002a3cce
nakaya-kouhei <northsea1224@gmail.com>
2017-10-17 13:46:39 +0900

    Java メソッド戻り値戻り値2課題 提出します

---
+++ CampChallenge_java/011.メソッド/GEEKJOB_Challenge11-3-2/src/java/org/camp/servlet/return2.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class return2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    //  人物の情報を配列に格納して返却するメソッド
+    ArrayList<String> profile(){
+        
+        ArrayList<String> profile = new ArrayList<String>();
+        
+        //  ID
+        profile.add("01");
+        //  名前
+        profile.add("座波");
+        //  生年月日
+        profile.add("1995年5月23日");
+        //  住所
+        profile.add("東京都世田谷区用賀4丁目10-1");
+        
+        return profile;
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            ArrayList<String> profile = new ArrayList<String>();
+
+            //  メソッドを呼び出して配列を受け取る
+            profile = profile();
+            
+            //  受け取った配列から、ID以外を表示
+            for(int i=1; i<=profile.size(); i++){
+                
+                out.println(profile.get(i) + "<br>");
+                
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
